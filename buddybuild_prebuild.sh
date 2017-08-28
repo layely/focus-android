d13149218c145e8381004d2539ab1db0ad1dce12
Sebastian Kaspari <s.kaspari@gmail.com>
2017-08-28 11:51:06 +0200

    Disable check_translations.py for release temporarily.

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -3,4 +3,5 @@
 # Pre-build script for buddybuild
 # http://docs.buddybuild.com/docs/custom-prebuild-and-postbuild-steps#section-pre-build
 
-python tools/l10n/check_translations.py
+# https://github.com/mozilla-mobile/focus-android/issues/1186
+# python tools/l10n/check_translations.py
